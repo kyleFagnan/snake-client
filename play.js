c9dc31dd402d9d7d5c3f02ce2d6a2116cc37b7a5
Kyle Fagnan <kfagnan19@gmail.com>
2021-11-03 15:09:45 -0600

    refactor play and input

--- play.js
+++ play.js
@@ -1,19 +1,9 @@
-const myObject = require("./client.js");
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
+console.log("Connecting ...");
 
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
 
 setupInput();
+
